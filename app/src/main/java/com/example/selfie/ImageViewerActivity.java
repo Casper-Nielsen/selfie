21ed10ad23e90b840ce7a499a3cb0f1e3b329eac
Casper Koksby Møller Nielsen <casp8660@zbc.dk>
2021-05-03 12:46:12 +0200

    added strings

--- app/src/main/java/com/example/selfie/ImageViewerActivity.java
+++ app/src/main/java/com/example/selfie/ImageViewerActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ImageViewerActivity extends AppCompatActivity {
     private Bitmap image;
@@ -18,5 +20,12 @@ public class ImageViewerActivity extends AppCompatActivity {
         image = (Bitmap) intent.getExtras().get("image");
         ImageView imageView = findViewById(R.id.imageView);
         imageView.setImageBitmap(image);
+    }
+
+    public void onSaveButtonClick(View view){
+        Toast.makeText(this, "not implemented", Toast.LENGTH_SHORT).show();
+    }
+    public void onBackButtonClick(View view){
+        startActivity(new Intent(this,MainActivity.class));
     }
 }
