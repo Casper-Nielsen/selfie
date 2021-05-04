b0a38121b1f3173f6a908ae61abf3c7115403ceb
Casper Koksby Møller Nielsen <casp8660@zbc.dk>
2021-05-04 08:56:41 +0200

    updated with comments

--- app/src/main/java/com/example/selfie/ImageViewerActivity.java
+++ app/src/main/java/com/example/selfie/ImageViewerActivity.java
@@ -9,6 +9,15 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+/**
+ * Activity for loading layout resources
+ *
+ * This activity is used to display a image
+ *
+ * @author CKN
+ * @version 1.1
+ * @since 1.0
+ */
 public class ImageViewerActivity extends AppCompatActivity {
     private Bitmap image;
 
@@ -16,15 +25,24 @@ public class ImageViewerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_viewer);
+
+        // Gets the image from the intent
         Intent intent = getIntent();
         image = (Bitmap) intent.getExtras().get("image");
         ImageView imageView = findViewById(R.id.imageView);
         imageView.setImageBitmap(image);
     }
 
+    /**
+     * Method that is for the button click to save the image
+     */
     public void onSaveButtonClick(View view){
         Toast.makeText(this, "not implemented", Toast.LENGTH_SHORT).show();
     }
+
+    /**
+     * Method that is for the button click to go back
+     */
     public void onBackButtonClick(View view){
         startActivity(new Intent(this,MainActivity.class));
     }
--- app/src/main/java/com/example/selfie/MainActivity.java
+++ app/src/main/java/com/example/selfie/MainActivity.java
@@ -10,7 +10,17 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+/**
+ * Activity for loading layout resources
+ *
+ * This activity is used to display buttons that will open the camera app
+ *
+ * @author CKN
+ * @version 1.1
+ * @since 1.0
+ */
 public class MainActivity extends AppCompatActivity {
     private int REQUEST_IMAGE_CAPTURE = 1;
 
@@ -19,25 +29,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * Method that is for the button click to open the camera
+     */
     public void onButtonTakeClicked(View view){
         dispatchTakePictureIntent();
     }
 
+    /**
+     * Method that opens the camera
+     */
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         try {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         } catch (ActivityNotFoundException e) {
-            // display error state to the user
+            Toast.makeText(this, "error can't open the camera", Toast.LENGTH_SHORT).show();
+
         }
     }
 
+    /**
+     * Method that takes the data from the camera and sends it to another activity
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            // Gets the preview
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            // Opens the other activity
             Intent intent = new Intent(this,ImageViewerActivity.class);
             intent.putExtra("image",imageBitmap);
             startActivity(intent);
